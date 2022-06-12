3b3ff625a91bc43bdbf445be6f19b76ab47b8d40
Srabo93 <a.s-dev@web.de>
2022-06-12 11:36:50 +0200

    setup ChakraUI

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme";
+import "../src/theme/styles.css";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
