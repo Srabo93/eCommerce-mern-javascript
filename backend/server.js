9a02f3c38198a32a4c16fe580c9b4d2a30040fce
Srabo93 <a.s-dev@web.de>
2022-06-16 21:01:33 +0200

    added ES module imports

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
-const express = require('express')
-const products = require('./data/products')
+import express from 'express'
+import products from './data/products.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 
@@ -15,4 +18,7 @@ app.get('/api/products/:id', (req,res)=>{
 	const product = products.find(product => product._id === req.params.id)
 	res.json(product)
 })
-app.listen(5000, console.log('Server is running on port 5000'))
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`))
