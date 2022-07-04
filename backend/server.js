9239b494f0759ddc0b39f18f46cf59489c2b78a1
Srabo93 <a.s-dev@web.de>
2022-07-04 15:23:07 +0200

    Added Mongoose and Connect to DB

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,33 @@
-import express from 'express'
-import products from './data/products.js'
-import dotenv from 'dotenv'
+import express from "express";
+import products from "./data/products.js";
+import colors from "colors";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+connectDB();
 
-app.get('/', (req,res)=> {
-	res.send('API is running')
-})
+const app = express();
 
-app.get('/api/products',(req,res)=>{
-	res.json(products)
-})
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
 
-app.get('/api/products/:id', (req,res)=>{
-	const product = products.find(product => product._id === req.params.id)
-	res.json(product)
-})
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
 
-const PORT = process.env.PORT || 5000
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((product) => product._id === req.params.id);
+  res.json(product);
+});
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`))
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
+  )
+);
