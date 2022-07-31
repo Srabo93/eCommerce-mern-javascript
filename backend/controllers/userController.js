05daa5114078178a88052b08d5712ba7e37be4a0
Srabo93 <a.s-dev@web.de>
2022-07-31 17:42:46 +0200

    added protectet routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,7 +11,7 @@ const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
   const user = await User.findOne({ email: email });
-  console.log(user);
+
   if (user && (await user.matchPassword(password))) {
     res.json({
       _id: user._id,
@@ -26,4 +26,25 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser };
+/**
+ * @description Get User Profile
+ * @route GET /api/users/profile
+ * @access Private
+ */
+
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+export { authUser, getUserProfile };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { authUser } from "../controllers/userController.js";
+import { authUser, getUserProfile } from "../controllers/userController.js";
+import protect from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
+router.route("/profile").get(protect, getUserProfile);
 router.route("/login").post(authUser);
 
 export default router;
