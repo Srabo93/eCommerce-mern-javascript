39938b445cc67b56a38a7d070b1fbee89ffdf528
Srabo93 <a.s-dev@web.de>
2022-08-21 12:11:31 +0200

    added a checkoutBar indicating the checkout steps

---
+++ frontend/src/components/CheckOutSteps.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink } from "@chakra-ui/react";
+import { ChevronRightIcon } from "@chakra-ui/icons";
+
+const CheckOutSteps = ({ step1, step2, step3, step4 }) => {
+  return (
+    <Breadcrumb separator={<ChevronRightIcon color="gray.500" />} pb={3}>
+      <BreadcrumbItem>
+        {step1 ? (
+          <BreadcrumbLink as={Link} to="/login">
+            Sign In
+          </BreadcrumbLink>
+        ) : (
+          <BreadcrumbLink
+            color="gray"
+            style={{ textDecoration: "none" }}
+            _hover={{ cursor: "default" }}
+          >
+            Sign In
+          </BreadcrumbLink>
+        )}
+      </BreadcrumbItem>
+      <BreadcrumbItem>
+        {step2 ? (
+          <BreadcrumbLink as={Link} to="/shipping">
+            Shipping
+          </BreadcrumbLink>
+        ) : (
+          <BreadcrumbLink
+            color="gray"
+            style={{ textDecoration: "none" }}
+            _hover={{ cursor: "default" }}
+          >
+            Shipping
+          </BreadcrumbLink>
+        )}
+      </BreadcrumbItem>
+      <BreadcrumbItem>
+        {step3 ? (
+          <BreadcrumbLink as={Link} to="/payment">
+            Payment
+          </BreadcrumbLink>
+        ) : (
+          <BreadcrumbLink
+            color="gray"
+            style={{ textDecoration: "none" }}
+            _hover={{ cursor: "default" }}
+          >
+            Payment
+          </BreadcrumbLink>
+        )}
+      </BreadcrumbItem>
+      <BreadcrumbItem>
+        {step4 ? (
+          <BreadcrumbLink as={Link} to="/placeorder">
+            Place Order
+          </BreadcrumbLink>
+        ) : (
+          <BreadcrumbLink
+            color="gray"
+            style={{ textDecoration: "none" }}
+            _hover={{ cursor: "default" }}
+          >
+            Place Order
+          </BreadcrumbLink>
+        )}
+      </BreadcrumbItem>
+    </Breadcrumb>
+  );
+};
+
+export default CheckOutSteps;
