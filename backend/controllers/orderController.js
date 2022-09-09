4e0dec4f331030d9cfa858d6b968c9033d7313ca
Srabo93 <A.S-dev@web.de>
2022-09-09 20:08:31 +0200

    added updateOrder when paid functionality

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -65,4 +65,30 @@ const getOrderById = asyncHandler(async (req, res) => {
   res.json(order);
 });
 
-export { addOrderItems, getOrderById };
+/**
+ * @description Update Order to Paid
+ * @route PUT /api/order/:id/pay
+ * @access private
+ */
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (!order) {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+
+  order.isPaid = true;
+  order.paidAt = Date.now();
+  order.paymentResult = {
+    id: req.body.id,
+    status: req.body.status,
+    update_time: req.body.update_time,
+    email_address: req.body.payer.email_adress,
+  };
+
+  const updatedOrder = await order.save();
+
+  res.json(updatedOrder);
+});
+export { addOrderItems, getOrderById, updateOrderToPaid };
