abf0e988376b64448f34d91266d50de6d9ceb935
Srabo93 <a.s-dev@web.de>
2022-10-15 17:05:57 +0200

    added Functionality to create New Product Button

--- frontend/src/features/ProductListScreen.js
+++ frontend/src/features/ProductListScreen.js
@@ -1,9 +1,10 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import {
   selectAllProducts,
   useDeleteProductMutation,
+  useCreateProductMutation,
 } from "./services/products";
 import {
   Button,
@@ -19,16 +20,31 @@ import {
   Container,
 } from "@chakra-ui/react";
 import { EditIcon, DeleteIcon, AddIcon } from "@chakra-ui/icons";
+import Loader from "../components/Loader";
 
 const ProductListScreen = () => {
+  const navigate = useNavigate();
   const products = useSelector(selectAllProducts);
   const [deleteProduct] = useDeleteProductMutation();
+  const [createProduct, { isLoading, data }] = useCreateProductMutation();
 
-  let tBody;
+  useEffect(() => {
+    if (data !== undefined) {
+      navigate(`/admin/product/${data?._id}/edit`);
+    }
+
+    return () => {};
+  }, [data]);
 
   const deleteHandler = async (id) => {
     await deleteProduct(id);
   };
+
+  const createProductHandler = async () => {
+    await createProduct();
+  };
+
+  let tBody;
 
   tBody = products.map((product) => (
     <Tr key={product._id}>
@@ -55,6 +71,7 @@ const ProductListScreen = () => {
       <Heading py={3} as="h2" mr="auto">
         PRODUCTS
       </Heading>
+      {isLoading && <Loader />}
       <TableContainer maxW="100vw">
         <Table variant="simple" size="small">
           <TableCaption>All Products</TableCaption>
@@ -66,11 +83,14 @@ const ProductListScreen = () => {
               <Th p={2}>CATEGORY</Th>
               <Th p={2}>BRAND</Th>
               <Th p={2}>
-                <Link to="/">
-                  <Button leftIcon={<AddIcon />} variant="ghost" color="teal">
-                    New Product
-                  </Button>
-                </Link>
+                <Button
+                  leftIcon={<AddIcon />}
+                  variant="ghost"
+                  color="teal"
+                  onClick={createProductHandler}
+                >
+                  New Product
+                </Button>
               </Th>
             </Tr>
           </Thead>
