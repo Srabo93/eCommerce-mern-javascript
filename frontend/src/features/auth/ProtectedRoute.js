9433327e84fdc4a80f0eca27644ec1d002cc433c
Srabo93 <A.S-dev@web.de>
2022-10-23 19:46:14 +0200

    removed unnecessary param from function

--- frontend/src/features/auth/ProtectedRoute.js
+++ frontend/src/features/auth/ProtectedRoute.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
-const ProtectedRoute = ({ isAllowed, redirectPath = "/", children }) => {
+const ProtectedRoute = ({ isAllowed, redirectPath = "/" }) => {
   if (!isAllowed) {
     return <Navigate to={redirectPath} replace />;
   }
