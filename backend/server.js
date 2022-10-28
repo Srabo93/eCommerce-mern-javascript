2b29f5474aa0c1196e6e78b35f93cbc70e54cc52
Srabo93 <a.s-dev@web.de>
2022-10-28 22:13:17 +0200

    removed console.log message

--- backend/server.js
+++ backend/server.js
@@ -71,7 +71,7 @@ const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
-  console.log("inside function");
+
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
