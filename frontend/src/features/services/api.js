3de312d94861fc2f58a38d6dace0c37ec8564f69
Srabo93 <A.S-dev@web.de>
2022-10-30 17:24:48 +0100

    added backend api

--- frontend/src/features/services/api.js
+++ frontend/src/features/services/api.js
@@ -5,7 +5,7 @@ export const api = createApi({
   refetchOnFocus: true,
   refetchOnReconnect: true,
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api",
+    baseUrl: "https://proshop-23tk.onrender.com//api",
     prepareHeaders: (headers) => {
       const token = JSON.parse(localStorage.getItem("authorization"));
 
