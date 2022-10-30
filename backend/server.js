55facaebfd00fd7f6677165a738110bad9e5becc
Srabo93 <A.S-dev@web.de>
2022-10-30 16:50:34 +0100

    test

--- backend/server.js
+++ backend/server.js
@@ -73,7 +73,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile("index.html", path.join(__dirname, "frontend", "build"))
   );
 } else {
   app.get("/", (req, res) => {
