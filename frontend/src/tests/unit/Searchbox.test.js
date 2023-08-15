66ec00d9758edaf795945f41681ae2425efba83b
srabo93 <a.s-dev@web.de>
2023-08-15 23:13:01 +0200

    Searchbox Unittest

---
+++ frontend/src/tests/unit/Searchbox.test.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { useNavigate } from "react-router-dom";
+import Searchbox from "../../components/Searchbox";
+
+// Mocking the useNavigate hook
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: jest.fn(),
+}));
+
+describe("Searchbox component", () => {
+  it("renders input and search button", () => {
+    render(<Searchbox />);
+
+    expect(screen.getByPlaceholderText("Search Product")).toBeInTheDocument();
+    expect(screen.getByText("Search")).toBeInTheDocument();
+  });
+
+  it("updates keyword state on input change", () => {
+    render(<Searchbox />);
+
+    const input = screen.getByPlaceholderText("Search Product");
+    fireEvent.change(input, { target: { value: "shoes" } });
+
+    expect(input.value).toBe("shoes");
+  });
+
+  it("navigates to search results on search button click", () => {
+    const navigate = jest.fn();
+    useNavigate.mockReturnValue(navigate);
+
+    render(<Searchbox />);
+
+    const input = screen.getByPlaceholderText("Search Product");
+    const searchButton = screen.getByText("Search");
+
+    fireEvent.change(input, { target: { value: "shoes" } });
+    fireEvent.click(searchButton);
+
+    expect(navigate).toHaveBeenCalledWith("/search/shoes");
+  });
+
+  it("does not navigate if keyword is empty", () => {
+    const navigate = jest.fn();
+    useNavigate.mockReturnValue(navigate);
+
+    render(<Searchbox />);
+
+    const searchButton = screen.getByText("Search");
+
+    fireEvent.click(searchButton);
+
+    expect(navigate).not.toHaveBeenCalled();
+  });
+});
